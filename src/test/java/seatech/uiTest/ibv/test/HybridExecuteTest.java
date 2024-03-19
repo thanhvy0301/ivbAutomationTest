605c2a06804dd61a688144e369497ee66d152796
thanhvy0301 <vythanh064@gmail.com>
2024-03-19 09:38:09 +0700

    update setup allure report

--- src/test/java/seatech/uiTest/ibv/test/HybridExecuteTest.java
+++ src/test/java/seatech/uiTest/ibv/test/HybridExecuteTest.java
@@ -1,5 +1,6 @@
 package seatech.uiTest.ibv.test;
 
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
--- src/test/java/seatech/uiTest/util/HybridDriven.java
+++ src/test/java/seatech/uiTest/util/HybridDriven.java
@@ -1,25 +1,18 @@
 package seatech.uiTest.util;
 
 import com.fasterxml.jackson.databind.exc.InvalidFormatException;
-import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.Select;
-import org.testng.Assert;
-import org.testng.annotations.DataProvider;
 import org.testng.asserts.SoftAssert;
 import seatech.common.baseBrowser.Base;
-import seatech.common.functions.CommonFunctions;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.Properties;
 
 public class HybridDriven {
@@ -163,7 +156,7 @@ public class HybridDriven {
                         else if (action.equalsIgnoreCase("verifyText")) {
                             String actualText = driver.findElement(By.xpath(locatorValue)).getText();
                             softAssert.assertEquals(actualText, verify);
-                            //Log.info("Actual Text: " +actualText + "Expected Text: "+verify +"Verification passed");
+                            Log.info("Actual Text: "+actualText);
                         }
                         locatorType = null;
                         break;
